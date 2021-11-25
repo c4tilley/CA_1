17495c05c14382f43c39b89dc54c063820e73155
C Tilley <L00170308@student.lyit.ie>
2021-11-25 12:44:56 +0000

    file update

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -9,16 +9,16 @@
 #
 """
 
-html_doc = """<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+import urllib.request
+with urllib.request.urlopen('http://192.168.11.130') as response:
+    html = response.read()
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
 
-<p class="story">...</p>
-"""
+#print(soup.text.lower())
 
+#for link in soup.find_all('a'):
+    #print(link.get('href'))
+
+#print (soup.findAll('b'))
